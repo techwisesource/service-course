5b9ec3edbc44eaef9635fedf57772609803cb2f2
aryandh.a.w <aryandh.a.w@gmail.com>
2024-10-03 22:40:31 +0700

    [course] api store course

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use App\Models\Mentor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CourseController extends Controller
+{
+    public function store(Request $request)
+    {
+        $rules = [
+            "name" => "required|string",
+            "certificate" => "required|boolean",
+            "thumbnail" => "string|url",
+            "type" => "string|in:free,premium",
+            "status" => "string|in:draft,published",
+            "price" => "integer",
+            "level" => "string|in:all-level,beginner,intermediate,advance",
+            "mentor_id" => "required|integer",
+            "description" => "string"
+        ];
+
+        $data = $request->all();
+        $validator = Validator::make($data, $rules);
+
+        if($validator->fails()) {
+            return response()->json([
+                "status" => "error",
+                "message" => $validator->errors()
+            ], 400);
+        }
+
+        $mentorId = $request->input("mentor_id");
+        $mentor = Mentor::find($mentorId);
+        if(!$mentor) {
+            return response()->json([
+                "status" => "error",
+                "message" => "mentor not found"
+            ], 404);
+        }
+
+        $course = Course::create($data);
+        return response()->json([
+            "status" => "success",
+            "data" => $course
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\MentorController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // php artisan route:list --path=api
 
 Route::apiResource("/mentors", MentorController::class);
+Route::apiResource("/courses", CourseController::class);
