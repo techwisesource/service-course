476c3e12c5a7c617222cc0bfc313923b36d1dad6
aryandh.a.w <aryandh.a.w@gmail.com>
2024-10-04 15:39:09 +0700

    [chapter] api store

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\MentorController;
 use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
@@ -9,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource("/mentors", MentorController::class);
 Route::apiResource("/courses", CourseController::class);
+Route::apiResource("/chapters", ChapterController::class);
